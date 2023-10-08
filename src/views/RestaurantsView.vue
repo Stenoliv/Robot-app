ec81120194759cd23457d514c8bad8faecf1acf6
TheHermes <hkvheik@gmail.com>
2023-10-08 22:12:31 +0300

    Some testing

--- src/views/RestaurantsView.vue
+++ src/views/RestaurantsView.vue
@@ -4,6 +4,20 @@ import Restaurants from '../components/Restaurants.vue';
 export default {
     components: {
         Restaurants
+    },
+    data() {
+        return {
+            title: "Restaurants for your pleasure!",
+            res: [
+                {restaurant: "Unicafe Oliva", place:"Centrum"},
+                {restaurant: "Unicafe Kaivopiha", place:"Centrum"},
+                {restaurant: "McDonalds", place:"Centrum"},
+                {restaurant: "Subway", place:"Centrum"},
+                {restaurant: "Umeshu", place:"Centrum"},
+                {restaurant: "Luckiefun", place:"Centrum"},
+                {restaurant: "Burger King", place:"Centrum"},
+            ]
+        }
     }
 }
 </script>
@@ -12,8 +26,14 @@ export default {
     <RouterLink to="/">
         <span>Back</span>
     </RouterLink>
-    <h1>Restaurant</h1>
+    <h1>{{ title }}</h1>
     <div>This is where the map comes</div>
+    <div id="map"></div>
+    <Restaurants
+    v-for="r in res" :key="r" v-bind="r"
+    />
+    <!--<button type="button" @click.prevent="findRestaurants">GetRestaurants</button>-->
+
 </template>
 
 <style scoped>
