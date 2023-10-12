a390325f88654be36396846e4763d7efd2cd6dbb
Rasmus Räihä <rasmus.raiha@hotmail.com>
2023-10-12 11:22:18 +0300

    Continued on recipe view

--- src/components/SingleRecipe.vue
+++ src/components/SingleRecipe.vue
@@ -1,3 +1,9 @@
+<template>
+    <div :ref="id" class="Recipe active" @click="toggleRecipe()">
+        <h2>{{ title }}</h2>
+    </div>
+</template>
+
 <script>
 import { getCurrentInstance } from 'vue'
 
@@ -8,51 +14,80 @@ export default {
     props: ['id','title'],
     setup() {
         const instance = getCurrentInstance()
+        const AnimationDelay = 500
+        const cssAnimationDelay = AnimationDelay + 'ms'
         return {
-            instance
+            instance,
+            AnimationDelay,
+            cssAnimationDelay
         }
     },
     methods: {
-        openRecipe() {
+        toggleRecipe() {
             const recipe = this.$refs[this.id]
             const container = this.instance.parent;
+
             if (recipe.classList.contains('big')) {
                 recipe.classList.remove('big')
                 
-                // Parent controls
+                // Parent div controls
                 container.refs.container.classList.remove('disableScroll')
                 container.refs.container.classList.add('allowScroll')
             } else {
                 recipe.classList.add('big')
                 
-                // Parent controls
+                // Parent div controls
                 container.refs.container.classList.add('disableScroll')
                 container.refs.container.classList.remove('allowScroll')
             }
-            recipe.scrollIntoView({ behavior: 'smooth' })
+
+            recipe.scrollIntoView({ behavior: 'instant'})
+
+            if (recipe.classList.contains('big')) {
+                const array = Array.from(container.refs.container.children)
+                setTimeout(() => {
+                    if (recipe.classList.contains('big')) {
+                        array.forEach( elem => {
+                            if (elem != recipe) {
+                                elem.classList.add('hide')
+                                elem.classList.remove('active')
+                            }
+                        })
+                    }
+                }, this.AnimationDelay)
+            } else {
+                const array = Array.from(container.refs.container.children)
+                array.forEach( elem => {
+                    if (elem != recipe) {
+                        elem.classList.remove('hide')
+                        elem.classList.add('active')
+                    }
+                });
+            }
         }
     }
 }
-</script>
 
-<template>
-    <div :ref="id" class="Recipe" @click="openRecipe()">
-        <h2>{{ title }}</h2>
-    </div>
-</template>
+</script>
 
 <style scoped>
     * {
         color: black;
         --recipeGap: 50px;
+        --transition: v-bind('cssAnimationDelay');
     }
 
     .big {
-        height: calc(84vh - var(--recipeGap) / 2);
+        height: calc(86vh - var(--recipeGap));
+    }
+    .hide {
+        display: none;
+    }
+    .active {
+        display: flex;
     }
 
     .Recipe {
-        display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
@@ -63,7 +98,7 @@ export default {
         margin-right: auto;
         border-radius: 20px;
         box-shadow: 0 0 30px black;
-        transition: 450ms;
+        transition: var(--transition) ease-in-out;
     }
     .Recipe:hover:not(.big) {
         scale: 1.02;
