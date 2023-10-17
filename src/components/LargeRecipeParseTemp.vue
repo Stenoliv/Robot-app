e93a0bad357a75e4470eb24d07e1033a4339b978
Stenoliv <stenbaco@arcada.fi>
2023-10-17 13:40:12 +0300

    Parsin for recepies beginning
    First parts of parsing for recepies

---
+++ src/components/LargeRecipeParseTemp.vue
@@ -0,0 +1,13 @@
+<script>
+        recepieComponents = JSON.parse(Response)
+        title = recepieComponents.title
+        preparationtime = recepieComponents.readyInMinutes
+        extendedIngredients.forEach(ingredient => {
+            document.getElementById("aine").innerHTML = ingredient.original
+        });
+
+    
+</script>
+<template>
+    <div class="aine"></div>
+</template>
