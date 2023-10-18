7a561247272e2ec418662714789d2137782bd648
Stenoliv <stenbaco@arcada.fi>
2023-10-18 16:00:05 +0300

    Ingredientlist p1

--- src/components/SingleRecipe.vue
+++ src/components/SingleRecipe.vue
@@ -15,11 +15,10 @@
                         <h2>{{ title }}</h2>
                     </div>
                 </div>
-                <div v-show="loading" class="loading" >
-                    <div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
-                </div>
-                <div v-show="!loading" class="recipeInstructions">
-                    <InstructionStepView v-for="(step, key, index) in information.analyzedInstructions[0].steps"
+                <div class="recipeInstructions">
+                    <span v-for="(original, key, index) in extendedIngredients[0].original" key:index>{{ original }}</span>
+
+                    <InstructionStepView v-for="(step, key, index) in analyzedInstructions[0].steps"
                     :key="index"
                     v-bind="step"/>
                 </div>
@@ -35,9 +34,9 @@ import axios from 'axios'
 
 export default {
     components: {
-        InstructionStepView
-    },
-    props: ['id','title','image','imageType'],
+    InstructionStepView,
+},
+    props: ['id','title','image','imageType','original','analyzedInstructions','extendedIngredients'],
     data() {
         return {
             selected: false,
@@ -121,6 +120,7 @@ export default {
                     this.information = response.data
                     this.loading = false
                     console.log("steps", this.information.analyzedInstructions[0].steps)
+                    console.log("ingredients",this.information.extendedIngredients[0].original)
                 })
                 .catch((error) => {
                     console.log(error)
@@ -268,62 +268,4 @@ export default {
         height: 100%;
         overflow: auto;
     }
-
-    /* lånad kod från https://loading.io/css/ */
-    .lds-ellipsis {
-    display: inline-block;
-    position: relative;
-    width: 80px;
-    height: 80px;
-    }
-    .lds-ellipsis div {
-    position: absolute;
-    top: 33px;
-    width: 13px;
-    height: 13px;
-    border-radius: 50%;
-    background: black;
-    animation-timing-function: cubic-bezier(0, 1, 1, 0);
-    }
-    .lds-ellipsis div:nth-child(1) {
-    left: 8px;
-    animation: lds-ellipsis1 0.6s infinite;
-    }
-    .lds-ellipsis div:nth-child(2) {
-    left: 8px;
-    animation: lds-ellipsis2 0.6s infinite;
-    }
-    .lds-ellipsis div:nth-child(3) {
-    left: 32px;
-    animation: lds-ellipsis2 0.6s infinite;
-    }
-    .lds-ellipsis div:nth-child(4) {
-    left: 56px;
-    animation: lds-ellipsis3 0.6s infinite;
-    }
-    @keyframes lds-ellipsis1 {
-    0% {
-        transform: scale(0);
-    }
-    100% {
-        transform: scale(1);
-    }
-    }
-    @keyframes lds-ellipsis3 {
-    0% {
-        transform: scale(1);
-    }
-    100% {
-        transform: scale(0);
-    }
-    }
-    @keyframes lds-ellipsis2 {
-    0% {
-        transform: translate(0, 0);
-    }
-    100% {
-        transform: translate(24px, 0);
-    }
-    }
-
 </style>
